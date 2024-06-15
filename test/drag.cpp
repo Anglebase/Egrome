9fe927260d44aeda575ad2352ccde6d7c388fb7e
Anglebase <1628818324@qq.com>
2024-06-15 21:52:27 +0800

    添加可拖动对象基类

---
+++ test/drag.cpp
@@ -0,0 +1,58 @@
+#include "Drag.h"
+#include "App.h"
+#include "Painter.h"
+#include "Color.h"
+
+class DragBlock : public Drag
+{
+protected:
+    void paintEvent(const PaintEvent &event) override
+    {
+        auto &painter = event.beginPaint(this);
+        painter.setBrushColor(Color::White);
+        painter.drawFillRect(painter.rect());
+        event.endPaint();
+    }
+
+public:
+    DragBlock(const Rect &rect, Block *parent = nullptr)
+        : Drag(rect, parent)
+    {
+    }
+    ~DragBlock() {}
+};
+
+class Window : public Block
+{
+    DragBlock *drag;
+
+protected:
+    void paintEvent(const PaintEvent &event) override
+    {
+        auto &painter = event.beginPaint(this);
+        painter.setPenColor(Color::White);
+        painter.drawText({0,0},std::to_wstring(App::getFps()));
+        event.endPaint();
+        return Block::paintEvent(event);
+    }
+public:
+    Window(const Rect &rect, Block *parent = nullptr)
+        : Block(rect, parent)
+    {
+        drag = new DragBlock({100, 100, 200, 200}, this);
+        // drag->setEnabled(true, false);
+        drag->setReference(Ref::Window);
+    }
+    ~Window()
+    {
+        delete drag;
+    }
+};
+
+int main()
+{
+    Window window({0, 0, 1900, 1000});
+    App app{&window};
+    app.run();
+    return 0;
+}
